f67d7e9685369c45f7558a843c128a37eac3b391
YURADEAD <pozdneev1@gmail.com>
2024-04-09 20:02:44 -0400

    Simple navbar started

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
           <Navbar/>
-          {children}
+          <main className="relative overflow-hidden">
+            {children}
+          </main>
           <Footer/>
       </body>
     </html>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,25 @@
 "use client"
-import React from 'react'
+
+import { NAV_LINKS } from "@/constants"
+import Image from "next/image"
+import Link from "next/link"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="border-2 border-red-500 flexBetween max-container padding-container relative z-300 py-5">
+      <Link href="/">
+        <Image src="/GRAiD.png" alt="logo" width={74} height={29}/>
+      </Link>
+
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map((link) => (
+          <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+      
+    </nav>
   )
 }
 
