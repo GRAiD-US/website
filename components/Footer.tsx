cf34a63406dd5f4679448c464a49262560f1cc00
YURADEAD <pozdneev1@gmail.com>
2024-04-12 16:11:55 -0400

    fixed key in footer and ' in main

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,17 +6,15 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <section>
-      <footer className="flexCenter mb-24">
+    <footer className="flexCenter mb-24">
       <div
-        className="padding-container max-container flex
-      w-full flex-col gap-14"
+        className="padding-container max-container flex w-full flex-col gap-14"
       >
         <div
           className="flex flex-col items-start justify-center
         gap-[10%] md:flex-row"
         >
-          <Link href="/" className="mb-10">
+          <Link href="/" key = "GRAiD" className="mb-10">
             <Image src="/GRAiD.png" alt="logo" width={74} height={29} />
           </Link>
           <div
@@ -25,55 +23,53 @@ const Footer = () => {
           >
             {FOOTER_LINKS.map((columns) => (
               <FooterColumn title={columns.title}>
-                  <ul
-                    className="regular-14 flex flex-col gap-4
-                  text-gray-30"
-                  >
-                   {columns.links.map((link) => (
-                     <Link href="/" key={link}>
-                       {link}
-                      </Link>
-                    ))}
-                  </ul>
-                </FooterColumn>
-              ))}
-              <div className="flex flex-col gap-5">
-                <FooterColumn title={FOOTER_CONTACT_INFO.title}>
-                  {FOOTER_CONTACT_INFO.links.map((link) => (
-                    <Link
-                      href="/"
-                      key={link.label}
-                      className="flex gap-4 md:flex-col lg:flex-row"
-                    >
-                      <p className="whitespace-nowrap">{link.label}</p>
-                      <p className="medium-14 whitespace-nowrap text-blue-70">
-                        {link.value}
-                      </p>
+                <ul
+                  className="regular-14 flex flex-col gap-4
+                text-gray-30"
+                >
+                  {columns.links.map((link) => (
+                    <Link href="/" key={link}>
+                      {link}
                     </Link>
                   ))}
-                </FooterColumn>
-              </div>
-              <div className="flex flex-col gap-5">
-               <FooterColumn title={SOCIALS.title}>
-                  <ul className="regular-14 flex gap-4 text-gray-30">
-                    {SOCIALS.links.map((link) => (
-                      <Link href="/" key={link}>
-                        <Image src={link} alt="logo" width={24} height={24} />
-                      </Link>
-                    ))}
-                  </ul>
-                </FooterColumn>
-              </div>
+                </ul>
+              </FooterColumn>
+            ))}
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={FOOTER_CONTACT_INFO.title}>
+                {FOOTER_CONTACT_INFO.links.map((link) => (
+                  <Link
+                    href="/"
+                    key={link.label}
+                    className="flex gap-4 md:flex-col lg:flex-row"
+                  >
+                    <p className="whitespace-nowrap">{link.label}</p>
+                    <p className="medium-14 whitespace-nowrap text-blue-70">
+                      {link.value}
+                    </p>
+                  </Link>
+                ))}
+              </FooterColumn>
+            </div>
+            <div className="flex flex-col gap-5">
+              <FooterColumn title={SOCIALS.title}>
+                <ul className="regular-14 flex gap-4 text-gray-30">
+                  {SOCIALS.links.map((link) => (
+                    <Link href="/" key={link}>
+                      <Image src={link} alt="logo" width={24} height={24} />
+                    </Link>
+                  ))}
+                </ul>
+              </FooterColumn>
             </div>
           </div>
-          <div className="border bg-gray-20" />
-          <p className="regular-14 w-full text-center text-gray-30">
-            2024 GRAiD | All rights reserved
-          </p>
         </div>
-      </footer>
-    </section>
-    
+        <div className="border bg-gray-20" />
+        <p className="regular-14 w-full text-center text-gray-30">
+          2024 GRAiD | All rights reserved
+        </p>
+      </div>
+    </footer>
   );
 };
 
